1914712578ca865faefdc76b92bbf91a6be3252c
divyanshubansal <divyanshubansal.db@gmail.com>
2020-07-28 19:52:05 +0530

    changed the name

--- startup/logging.js
+++ startup/logging.js
@@ -13,7 +13,7 @@ module.exports = function() {
   
   winston.add(winston.transports.File, { filename: 'logfile.log' });
   // winston.add(winston.transports.MongoDB, { 
-  //   db: 'mongodb://localhost/rently',
+  //   db: 'mongodb://localhost/rentum',
   //   level: 'info'
   // });  
 }
